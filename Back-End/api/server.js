e11fcb9025303921d121462641a32df263ebda51
Pedro Henrique Netto Morona <pedromorona25@gmail.com>
2025-05-12 20:52:36 -0300

    PHtify Final-Project v0.2

--- Back-End/api/server.js
+++ Back-End/api/server.js
@@ -6,24 +6,34 @@
 import express from 'express'
 import cors from 'cors'
 import { db } from './connect.js'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 const app = express()
 const PORT = 3001
 
 app.use(cors())
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send("Olá, Mundo!")
 })
 
-app.get('/artists', async (request, response) => {
+app.get('/api/artists', async (request, response) => {
     response.send(await db.collection("artists").find({}).toArray())
 })
 
-app.get('/songs', async (request, response) => {
+app.get('/api/songs', async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray())
 })
 
-app.listen(PORT, () => {
-    console.log("Servidor rodando na porta 3001")
+app.use(express.static(path.join(__dirname, "../Front-End/dist")))
+
+app.get('*page', async (request, response) => {
+    response.sendFile(path.join(__dirname, "../Front-End/dist/index.html"))
 })
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
+
